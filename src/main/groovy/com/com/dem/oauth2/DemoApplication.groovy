bddcd7a4e8a41e84f7728f644d10659035a11f81
josdem <joseluis.delacruz@gmail.com>
2017-08-21 18:50:16 -0500

    Enabling EnableOAuth2Sso and reading yml from system

--- src/main/groovy/com/com/dem/oauth2/DemoApplication.groovy
+++ src/main/groovy/com/com/dem/oauth2/DemoApplication.groovy
@@ -2,8 +2,10 @@ package com.jos.dem.oauth2
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso
 
 @SpringBootApplication
+@EnableOAuth2Sso
 class DemoApplication {
 
 	static void main(String[] args) {
