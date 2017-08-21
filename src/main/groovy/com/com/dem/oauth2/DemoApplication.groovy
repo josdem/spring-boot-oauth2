2e90768be0d9a90420242e67a0bec1a0f91ebf2d
josdem <joseluis.delacruz@gmail.com>
2017-08-21 18:43:43 -0500

    Adding Spring security dependency

--- src/main/groovy/com/com/dem/oauth2/DemoApplication.groovy
+++ src/main/groovy/com/com/dem/oauth2/DemoApplication.groovy
@@ -1,4 +1,4 @@
-package com.example.springbootoauth2
+package com.jos.dem.oauth2
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
