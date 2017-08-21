23cb291923bbc86891b9e232878e1f279f69aa18
josdem <joseluis.delacruz@gmail.com>
2017-08-21 18:33:58 -0500

    Starting Spring Boot Oauth2 Demo Repository :)

---
+++ src/main/groovy/com/example/springbootoauth2/DemoApplication.groovy
@@ -0,0 +1,12 @@
+package com.example.springbootoauth2
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class DemoApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run DemoApplication, args
+	}
+}
