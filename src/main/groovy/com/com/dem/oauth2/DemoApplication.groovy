8ee76bb7d2881256dbe3020db0718957d6e6121b
josdem <joseluis.delacruz@gmail.com>
2017-08-24 17:37:51 -0500

    Adding controller and config

--- src/main/groovy/com/com/dem/oauth2/DemoApplication.groovy
+++ src/main/groovy/com/com/dem/oauth2/DemoApplication.groovy
@@ -1,30 +1,13 @@
 package com.jos.dem.oauth2
 
 import org.springframework.boot.SpringApplication
-import org.springframework.web.bind.annotation.RestController
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 
 @SpringBootApplication
-@EnableOAuth2Sso
-@RestController
-class DemoApplication extends WebSecurityConfigurerAdapter{
+class DemoApplication {
 
   static void main(String[] args) {
     SpringApplication.run DemoApplication, args
-  }
-
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
-    http
-    .antMatcher("/**")
-    .authorizeRequests()
-    .antMatchers("/", "/login**", "/webjars/**")
-    .permitAll()
-    .anyRequest()
-    .authenticated();
   }
 
 }
