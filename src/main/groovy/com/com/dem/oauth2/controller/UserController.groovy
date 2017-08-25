e16ac58a753a94c799f3debbc561ab2320a0e253
josdem <joseluis.delacruz@gmail.com>
2017-08-24 20:05:56 -0500

    Getting user auth information

--- src/main/groovy/com/com/dem/oauth2/controller/UserController.groovy
+++ src/main/groovy/com/com/dem/oauth2/controller/UserController.groovy
@@ -1,15 +1,25 @@
 package com.jos.dem.oauth2.controller
 
+import java.security.Principal
+
 import org.springframework.stereotype.Controller
+import org.springframework.web.servlet.ModelAndView
 import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
 @RequestMapping("/user")
 class UserController {
 
+  Logger log = LoggerFactory.getLogger(this.class)
+
   @RequestMapping('/show')
-  String show(){
-    'user/show'
+  ModelAndView show(Principal principal){
+    Map details = [:]
+    details.name = principal.name
+    details.email =  principal.userAuthentication?.details?.email
+    ModelAndView modelAndView = new ModelAndView('user/show')
+    modelAndView.addObject('details', details)
+    modelAndView
   }
 
 }
